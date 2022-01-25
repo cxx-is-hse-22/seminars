9893bb9a26b79eaddf9c5eda57110d78a2341de0
Alexey Kreshchuk <akrsch@gmail.com>
2022-01-25 13:05:43 +0300

    add unique_ptr

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "lib.hpp"
+#include <cassert>
 #include <fmt/core.h>
 #include <fmt/ranges.h>
 #include <iostream>
@@ -80,10 +81,11 @@ auto main() -> int { // or int main()
     // std::shared_ptr<Person>(new Person{"Joe", 33})
     // new использовать не надо
     auto ptr2 = ptr;
-    ptr2->student = false; // (*ptr2).student = false;
+    auto &person_ref = *ptr; // *ptr превращает (умный) указатель ptr в ссылку
+    (*ptr2).student = false;
+    ptr2->student = false; // Копия прошлой строки
     std::cout << "Joe student=" << ptr->student << "\n";
     print(*ptr);
-    auto &person_ref = *ptr;
     ptr = std::make_shared<Person>("Jack", 55);
     // На Joe остался 1 указатель
     print(person_ref);
@@ -94,13 +96,22 @@ auto main() -> int { // or int main()
     // Мы говорим, что shared_ptr "владеет" объектом, на который ссылается
     // А ссылка не "владеет" им
     // Пока жив "владелец", живы все его "владения"
+    auto vec = std::vector{1, 2, 3};
+    auto &vec0 = vec[0]; // NOLINT
+    vec = std::vector<int>{};
+    // vec0 теперь неправильный
 
-    struct List {
+    auto uptr = std::make_unique<Person>("Joe", 33);
+    auto &uperson_ref = *uptr;
+    uperson_ref.student = false;
+    uptr = nullptr;
+
+    struct ListElement {
         int value;
-        // auto std::make_shared<Type>(...) -> std::shared_ptr<Type>
-        std::shared_ptr<List> next; // лучше не так
-        List(int v) { value = v; }
-        auto print() -> void {
+        // auto std::make_unique<Type>(...) -> std::unique_ptr<Type>
+        std::unique_ptr<ListElement> next;
+        ListElement(int v) { value = v; }
+        auto print() const -> void {
             if (next) {
                 std::cout << value << ", ";
                 next->print();
@@ -109,33 +120,42 @@ auto main() -> int { // or int main()
         }
     };
 
-    auto list_ptr = std::make_shared<List>(1);
-    list_ptr->next = list_ptr;
-    // ^ Утечка памяти: list_ptr никогда не будет удалён
-    list_ptr->next = std::make_shared<List>(2);
+    auto list_ptr = std::make_unique<ListElement>(1);
+    list_ptr->next = std::make_unique<ListElement>(2);
     list_ptr->print();
     // дописали в конец, теперь список длины 2
     {
-        auto new_list_elem = std::make_shared<List>(3);
+        auto new_list_elem = std::make_unique<ListElement>(3);
         new_list_elem->print();
         // list_ptr --- это голова (начало) списка
-        new_list_elem->next = list_ptr;
+        // new_list_elem->next и list_ptr не могут одновременно владеть объектом
+        // 1
+        new_list_elem->next = std::move(list_ptr);
+        // Теперь list_ptr = nullptr (но это не точно)
+        // А new_list_elem->next теперь равен прошлому значению list_ptr
         new_list_elem->print();
-        list_ptr = new_list_elem;
+        std::swap(list_ptr, new_list_elem);
+        // ^ Меняет местами list_ptr и new_list_elem
+        // list_ptr, new_list_elem = new_list_elem, list_ptr
         list_ptr->print();
-    } // здесь удаляется указатель new_list_elem
+    } // здесь удаляется указатель new_list_elem (==nullptr)
     // дописали в начало, теперь список длины 3
 
-    struct List2 {
+    // list_ptr->next = std::move(list_ptr);
+    // ^ Утечка памяти: list_ptr никогда не будет удалён
+
+    struct List2Element {
         int value;
-        std::shared_ptr<List2> next;
+        std::shared_ptr<List2Element> next; // или std::unique_ptr
         // std::shared_ptr<List2> prev; // Неминуемая утечка памяти
         // list2_ref будет владеть собой через list2_ref->next->prev
-        std::weak_ptr<List2> prev;
-        List2(int v) { value = v; }
+        std::weak_ptr<List2Element> prev;
+        // ^ prev не владеет данными, на которые указывает
+        // ^ Для unique_ptr это будет List2* prev;
+        List2Element(int v) { value = v; }
     };
-    auto list2_ref = std::make_shared<List2>(1);
-    list2_ref->next = std::make_shared<List2>(2);
+    auto list2_ref = std::make_shared<List2Element>(1);
+    list2_ref->next = std::make_shared<List2Element>(2);
     list2_ref->next->prev = list2_ref;
     auto second = list2_ref->next;
     {
@@ -143,6 +163,30 @@ auto main() -> int { // or int main()
         if (second_prev)
             std::cout << "prev to second is " << second_prev->value << "\n";
     }
+
+    struct List2uElement {
+        int value;
+        std::unique_ptr<List2uElement> next;
+        List2uElement *prev = nullptr;
+        List2uElement(int v) { value = v; }
+    };
+
+    struct List2u {
+        std::unique_ptr<List2uElement> head;
+        List2uElement *tail = nullptr;
+
+        auto append(int value) {
+            if (head) {
+                assert(tail && !tail->next);
+                tail->next = std::make_unique<List2uElement>(value);
+                tail->next->prev = tail;
+                tail = tail->next.get();
+            } else {
+                head = std::make_unique<List2uElement>(value);
+                tail = head.get();
+            }
+        }
+    };
 
     struct Tree {
         std::shared_ptr<Tree> left, right; // лучше std::unique_ptr
