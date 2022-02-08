dedcb62e2a58abc9fc34a1e48ce903eb70bc9704
Alexey Kreshchuk <akrsch@gmail.com>
2022-02-08 11:18:32 +0300

    hash table implementation example

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,59 @@
 #include "lib.hpp"
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <forward_list>
+#include <string>
+#include <utility>
+#include <optional>
+
+using HashTable = std::vector<std::forward_list<std::pair<std::string, int>>>;
+
+void insert(HashTable &hashtable, std::string const& key, int value) {
+    std::hash<std::string> hasher;
+    auto &bucket = hashtable[hasher("a")];
+    bucket.emplace_front("a", 3);
+}
+
+auto find(HashTable const &hashtable, std::string const &key)
+    -> std::optional<std::pair<std::string, int>> {
+    std::hash<std::string> hasher;
+    auto const &bucket = hashtable[hasher("a")];
+    for (auto const &elem : bucket)
+        if (elem.first == key)
+            return elem;
+    return std::nullopt;
+}
+
+void resize(HashTable &hashtable, std::size_t newsize) {
+    HashTable newtable(newsize);
+    for (auto &bucket : hashtable)
+        for (auto &elem : bucket)
+            insert(newtable, elem.first, elem.second);
+}
+
+struct Bucket {
+    std::optional<std::pair<std::string, int>> content;
+    bool deleted = false;
+};
+using OpenHashTable = std::vector<Bucket>;
+void insert(OpenHashTable &hashtable, std::string const& key, int value) {
+    std::hash<std::string> hasher;
+    auto hash = hasher(key);
+    for (std::size_t i=0; i<hashtable.size(); ++i) {
+        auto &bucket = hashtable[(i + hash) % hashtable.size()];
+        if (bucket.content && bucket.content->first == key)
+            bucket.content->second = value;
+        else if (!bucket.content)
+            bucket.content = {key, value};
+    }
+    throw std::out_of_range("Hashtable is full");
+}
 
 int main() {
-  std::cout << not_random() << "\n";
-  return 0;
+    HashTable hashtable(127);
+
+
+
+    return 0;
 }
