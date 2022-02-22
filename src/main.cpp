d2173291568640ca7b56185a3bf4fd4636b8fe49
Alexey Kreshchuk <akrsch@gmail.com>
2022-02-22 12:52:21 +0300

    more algorithms

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ template <typename T> struct List {
 
 auto step01() {
     List<int> l1;
-    List<char> l2;
+    auto l2 = List<char>();
 }
 
 template <typename T> List<T> make_list123() {
@@ -37,7 +37,7 @@ template <typename T> List<T> make_listx3(T a, T b, T c) {
     return result;
 }
 
-// Продвинутая тема!
+// Продвинутая тема! (variadic template)
 template <typename T> List<T> make_list(T val) { return List<T>(val); }
 
 template <typename T, typename... Args>
@@ -45,6 +45,7 @@ List<T> make_list(T first, Args... rest) {
     // py: make_list(first, *args)
     // auto rest2 = rest; // error
     List<T> result{first};
+    // if constexpr (sizeof...(Args) > 0) // Можно и так прервать рекурсию
     result.next = std::make_unique<List<T>>(make_list(rest...));
     // py: make_list(*rest)
     return result;
@@ -56,7 +57,8 @@ auto smaller_than_4(int value) { return value < 4; }
 auto step02() {
     auto vec = std::vector{1, 2, 3, 4};
     auto sm4 = std::all_of(vec.begin(), vec.end(), smaller_than_4);
-    fmt::print("All is <4: {}\n", sm4);
+    auto sm4alt = std::all_of(vec.begin(), vec.begin() + 4, smaller_than_4);
+    fmt::print("All is <4: {} or {}\n", sm4, sm4alt);
     fmt::print("All positive: {}\n",
                std::all_of(vec.begin(), vec.end(),
                            [](int value) { return value > 0; }));
@@ -66,7 +68,7 @@ auto step02() {
                            [N](int value) { return value != N; }));
     // [N] - скопировать
     // [&N] - сохранить ссылку
-    // [&] - сохранить сслыки на всё
+    // [&] - сохранить ссылки на всё
     // [b = N] - скопировать N под именем b
     // [b = N*N] - создать переменную b со значением N*N
     // [=] - скопировать всё (нужно ли?)
@@ -85,6 +87,13 @@ auto step03() {
     it = vec.erase(it); // 2 удалено, it указывает на 3
     fmt::print("After erase: {}, it points to {} at pos {}\n", vec, *it,
                it - vec.begin());
+            //    std::distance(vec.begin(), it);
+    int sum = 0;
+    // for (auto c : vec) sum += c;
+    for (auto iter = vec.begin(); iter != vec.end(); ++iter) {
+        auto c = *iter;
+        sum += c;
+    }
     auto l = std::list{1, 2, 3, 4};
     for (auto lit = l.begin(); lit != l.end();)
         if (*lit % 2 == 0)
@@ -92,7 +101,7 @@ auto step03() {
         else
             ++lit;
     fmt::print("After loop with erase {}\n", l);
-    vec = std::vector{3, 2, 4, 5, 1};
+    vec = std::vector{3, 2, 4, 5, 1}; // NOLINT
     std::nth_element(vec.begin(), vec.begin() + 2, vec.end());
     // vec[2] == 3
     // vec[i] <= vec[2] для i < 2
@@ -103,8 +112,27 @@ auto step03() {
 void simple_sort(std::vector<int> &vec) {
     for (auto it = vec.begin(); it != vec.end(); ++it) {
         auto min_it = std::min_element(it, vec.end());
-        std::swap(*it, *min_it);
+        std::iter_swap(it, min_it);
     }
+}
+
+void bubble_sort(auto &vec) {
+    auto it = vec.begin();
+    auto next = it;
+    while ((next = std::next(it)) != vec.end()) {
+        if (*it > *next) {
+            std::iter_swap(it, next);
+            if (it != vec.begin()) --it;
+        } else
+            it = next;
+    }
+}
+
+auto find_if(auto begin, auto end, auto const &conditiion) {
+    for (auto iter = begin; iter!= end; ++iter)
+        if (conditiion(*iter))
+            return iter;
+    return end;
 }
 
 int main() {
@@ -114,5 +142,7 @@ int main() {
   l = make_list(1, 2, 3);
   step02();
   step03();
+  auto arr = std::vector{6,5,4,3,2,1};
+  simple_sort(arr);
   return 0;
 }
